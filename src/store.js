d357dc9922ca6af22e0035556e83efb9c2f4cc0b
hallelhel <hallelh@post.bgu.ac.il>
2023-06-20 22:27:05 +0300

    commit 1

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 
 export const state = {
 
-    server_domain: "http://localhost:3000",
+    // server_domain: "http://localhost:3000",
 
-    // server_domain: "http://132.72.65.211/",
+     server_domain: "http://assignment3-keren-ori.cs.bgu.ac.il",
     
 
 };
